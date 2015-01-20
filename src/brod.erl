5dc3384de11ab3ab6374f9ebe1a2f267c35b2446
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2015-01-20 15:56:39 +0100

    udpate function comment

--- src/brod.erl
+++ src/brod.erl
@@ -56,26 +56,26 @@ start_producer(Hosts) ->
 %%      Hosts:
 %%        list of "bootstrap" kafka nodes, {"hostname", 1234}
 %%      RequiredAcks:
-%%        How many acknowledgements the servers should receive
-%%        before responding to the request. If it is 0 the
-%%        server will not send any response (this is the only
-%%        case where the server will not reply to a request). If
-%%        it is 1, the server will wait the data is written to
-%%        the local log before sending a response. If it is -1
-%%        the server will block until the message is committed
-%%        by all in sync replicas before sending a response. For
-%%        any number > 1 the server will block waiting for this
-%%        number of acknowledgements to occur (but the server
-%%        will never wait for more acknowledgements than there
-%%        are in-sync replicas).
+%%        How many acknowledgements the kafka broker should receive
+%%        from the clustered replicas before responding to the
+%%        producer.
+%%        If it is 0 the broker will not send any response (this is
+%%        the only case where the broker will not reply to a
+%%        request). If it is 1, the broker will wait the data is
+%%        written to the local log before sending a response. If it is
+%%        -1 the broker will block until the message is committed by
+%%        all in sync replicas before sending a response. For any
+%%        number > 1 the broker will block waiting for this number of
+%%        acknowledgements to occur (but the broker will never wait
+%%        for more acknowledgements than there are in-sync replicas).
 %%      AckTimeout:
-%%        Maximum time in milliseconds the server can await the
+%%        Maximum time in milliseconds the broker can await the
 %%        receipt of the number of acknowledgements in
 %%        RequiredAcks. The timeout is not an exact limit on
 %%        the request time for a few reasons: (1) it does not
 %%        include network latency, (2) the timer begins at the
 %%        beginning of the processing of this request so if many
-%%        requests are queued due to server overload that wait
+%%        requests are queued due to broker overload that wait
 %%        time will not be included, (3) we will not terminate a
 %%        local write so if the local write time exceeds this
 %%        timeout it will not be respected.
