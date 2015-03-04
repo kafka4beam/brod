d45cb416a92b334d0ba3b4a311c6346814b57ceb
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2015-03-04 11:40:24 +0100

    add license header to every source file

--- src/brod_kafka.erl
+++ src/brod_kafka.erl
@@ -1,8 +1,24 @@
+%%%
+%%%   Copyright (c) 2014, 2015, Klarna AB
+%%%
+%%%   Licensed under the Apache License, Version 2.0 (the "License");
+%%%   you may not use this file except in compliance with the License.
+%%%   You may obtain a copy of the License at
+%%%
+%%%       http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%%   Unless required by applicable law or agreed to in writing, software
+%%%   distributed under the License is distributed on an "AS IS" BASIS,
+%%%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%%   See the License for the specific language governing permissions and
+%%%   limitations under the License.
+%%%
+
 %%%=============================================================================
 %%% @doc A kafka protocol implementation.
 %%%      [https://cwiki.apache.org/confluence/display/KAFKA/
 %%%       A+Guide+To+The+Kafka+Protocol].
-%%% @copyright 2014 Klarna AB
+%%% @copyright 2014, 2015 Klarna AB
 %%% @end
 %%%=============================================================================
 
