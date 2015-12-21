d88f076e0fd1b0798eb9abc3fb67f04c3ba8b2aa
Zaiming Shi <zmstone@gmail.com>
2015-12-21 13:42:01 +0100

    fix xref

--- src/brod_kafka_requests.erl
+++ src/brod_kafka_requests.erl
@@ -34,6 +34,7 @@
         , del/2
         , get_api_key/2
         , get_caller/2
+        , get_corr_id/1
         ]).
 
 -export_type([requests/0]).
@@ -89,6 +90,12 @@ get_caller(#requests{sent = Sent}, CorrId) ->
   {Caller, _ApiKey} = gb_trees:get(CorrId, Sent),
   Caller.
 
+
+%% @doc Get the correction to be sent for the next request.
+-spec get_corr_id(requests()) -> corr_id().
+get_corr_id(#requests{ corr_id = CorrId }) ->
+  CorrId.
+
 %%%_* Internal functions =======================================================
 
 next_corr_id(?MAX_CORR_ID) -> 0;
@@ -99,7 +106,10 @@ next_corr_id(CorrId)       -> CorrId + 1.
 -ifdef(TEST).
 
 next_corr_id_test() ->
-  ?assertEqual(0, next_corr_id((1 bsl 31) - 1)).
+  CorrId = (1 bsl 31) - 1,
+  Req = #requests{corr_id = CorrId},
+  {CorrId, NewReq} = add(Req, self(), api_key),
+  ?assertEqual(0, get_corr_id(NewReq)).
 
 -endif. % TEST
 
