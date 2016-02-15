6d6bb6d85a37ea3af7457b1e0275b9049a5112fa
Zaiming Shi <zmstone@gmail.com>
2016-02-15 15:45:01 +0100

    fix poking script

--- scripts/setup-1-node-test-env.sh
+++ scripts/setup-1-node-test-env.sh
@@ -7,7 +7,16 @@ cd $THIS_DIR/../docker
 sudo docker-compose -f docker-compose-basic.yml build
 sudo docker-compose -f docker-compose-kafka-1.yml up -d
 
-n=0; while [ "$(sudo docker exec kafka_1 bash -c '/opt/kafka/bin/kafka-topics.sh --zookeeper localhost --list')" != '' ]; do if [ $n -gt 4 ]; then echo timeout; exit 1; fi; n=$(( n + 1 )); sleep 1; done
+n=0
+while [ "$(sudo docker exec kafka_1 bash -c '/opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper --list')" != '' ]; do
+  if [ $n -gt 4 ]; then
+    echo timeout
+    exit 1
+  fi
+  n=$(( n + 1 ))
+  sleep 1
+done
+
 sudo docker exec kafka_1 bash -c "/opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper --create --partitions 1 --replication-factor 1 --topic brod-cli-produce-test"
 sudo docker exec kafka_1 bash -c "/opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper --create --partitions 1 --replication-factor 1 --topic brod-client-SUITE-topic"
 sudo docker exec kafka_1 bash -c "/opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper --create --partitions 1 --replication-factor 1 --topic brod_producer_SUITE"
