08e5e73ae93168d3687439e19ce922c93d64ecbc
Zaiming Shi <zmstone@gmail.com>
2016-02-15 18:10:58 +0100

    fix hostname

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -27,8 +27,14 @@ fi
 
 if [ ! -z "$KAFKA_PORT" ]; then
   echo "port: $KAFKA_PORT"
+  sed -r -i "s/^(port)=(.*)/\1=$KAFKA_PORT/g" $prop_file
   sed -r -i "s/^(listeners)=(.*)/\1=PLAINTEXT:\/\/:$KAFKA_PORT/g" $prop_file
 fi
+
+ipwithnetmask="$(ip -f inet addr show dev eth0 | awk '/inet / { print $2 }')"
+ipaddress="${ipwithnetmask%/*}"
+
+sed -r -i "s/^(advertised.host.name)=(.*)/\1=$ipaddress/g" $prop_file
 
 /opt/kafka/bin/kafka-server-start.sh $prop_file
 
