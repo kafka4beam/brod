81ffc695e60abfba6648f0218a580f202f374e95
Zaiming Shi <zmstone@gmail.com>
2016-02-25 22:35:18 +0100

    remove unused macors

--- src/brod_kafka.hrl
+++ src/brod_kafka.hrl
@@ -22,16 +22,6 @@
 
 -define(REPLICA_ID, -1).
 
-%% Api keys
--define(API_KEY_PRODUCE,        0).
--define(API_KEY_FETCH,          1).
--define(API_KEY_OFFSET,         2).
--define(API_KEY_METADATA,       3).
--define(API_KEY_LEADER_AND_ISR, 4).
--define(API_KEY_STOP_REPLICA,   5).
--define(API_KEY_OFFSET_COMMIT,  6).
--define(API_KEY_OFFSET_FETCH,   7).
-
 %% Compression
 -define(COMPRESS_NONE, 0).
 -define(COMPRESS_GZIP, 1).
