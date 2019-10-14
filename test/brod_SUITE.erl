abb71f078c81cf393896679cf20bd5a2d9a8f5d8
Ivy Rogatko <Ipr210lehigh@gmail.com>
2019-10-14 09:13:19 -0700

    removing 1 host and re-adding to CI

--- test/brod_SUITE.erl
+++ test/brod_SUITE.erl
@@ -33,7 +33,7 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
 
--define(HOSTS, [{"localhost", 9092}, {"localhost", 9192}]).
+-define(HOSTS, [{"localhost", 9092}]).
 -define(TOPIC, list_to_binary(atom_to_list(?MODULE))).
 -define(TIMEOUT, 50000).
 
