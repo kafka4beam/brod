6765b79f3faadc29bb9ce5b70f3c3062061639b5
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2022-08-05 12:29:51 -0300

    Fix kafka endpoint address on elixir sample

--- contrib/examples/elixir/config/dev.exs
+++ contrib/examples/elixir/config/dev.exs
@@ -4,7 +4,7 @@ config :brod,
   clients: [
     # You can choose the name of the client
     kafka_client: [
-      endpoints: ["kafka-default.dev.podium-dev.com": 19092],
+      endpoints: [{"localhost", 9092}],
       # This will auto-start the producers with default configs
       auto_start_producers: true
     ]
