077e6428f0b76770e6b0ac6096d0a9a871fdfbe6
zmstone <zmstone@gmail.com>
2025-11-19 15:14:05 +0100

    feat: add brod_utils:pmap/3

--- test/brod_utils_tests.erl
+++ test/brod_utils_tests.erl
@@ -53,6 +53,64 @@ make_batch_input_test_() ->
 
 mk(K, V) -> brod_utils:make_batch_input(K, V).
 
+pmap_test_() ->
+  [fun() ->
+    %% Basic functionality - simple map operation
+    Result = brod_utils:pmap(fun(X) -> X * 2 end, [1, 2, 3, 4, 5], infinity),
+    ?assertEqual([2, 4, 6, 8, 10], Result)
+   end,
+   fun() ->
+    %% Empty list
+    Result = brod_utils:pmap(fun(X) -> X end, [], infinity),
+    ?assertEqual([], Result)
+   end,
+   fun() ->
+    %% Order preservation - results should be in same order as input
+    Result = brod_utils:pmap(fun(X) -> X end, [3, 1, 4, 1, 5], infinity),
+    ?assertEqual([3, 1, 4, 1, 5], Result)
+   end,
+   fun() ->
+    %% Numeric transformation
+    Result = brod_utils:pmap(fun(X) -> X + 1 end, [10, 20, 30], infinity),
+    ?assertEqual([11, 21, 31], Result)
+   end,
+   fun() ->
+    %% Large list
+    List = lists:seq(1, 100),
+    Result = brod_utils:pmap(fun(X) -> X * 2 end, List, infinity),
+    Expected = [X * 2 || X <- List],
+    ?assertEqual(Expected, Result)
+   end,
+   fun() ->
+     %% Timeout behavior - function that takes longer than timeout
+     StartTime = erlang:monotonic_time(millisecond),
+     try
+       brod_utils:pmap(fun(X) -> timer:sleep(200), X * 2 end, [1, 2, 3], 50),
+       %% Should not reach here
+       ?assert(false)
+       catch
+         throw:timeout ->
+           EndTime = erlang:monotonic_time(millisecond),
+           %% Should timeout quickly (within timeout + small overhead)
+           ?assert(EndTime - StartTime < 100)
+     end
+   end,
+   fun() ->
+     %% Infinity timeout - should complete successfully
+     Result = brod_utils:pmap(fun(X) -> timer:sleep(10), X * 2 end, [1, 2, 3], infinity),
+     ?assertEqual([2, 4, 6], Result)
+   end,
+   fun() ->
+     %% Single element list
+     Result = brod_utils:pmap(fun(X) -> X + 100 end, [42], infinity),
+     ?assertEqual([142], Result)
+   end,
+   fun() ->
+     %% Function that returns different types
+     Result = brod_utils:pmap(fun(X) -> {result, X} end, [1, 2, 3], infinity),
+     ?assertEqual([{result, 1}, {result, 2}, {result, 3}], Result)
+   end].
+
 %%%_* Emacs ====================================================================
 %%% Local Variables:
 %%% allout-layout: t
